1d50b7f49bee97a7af1086496594f265f26a2001
YeshiNamkhai <yeshi.namkhai@gmail.com>
2019-09-10 21:58:00 +0200

    correzione

--- VacationPlanner.java
+++ VacationPlanner.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class VacationPlanner {
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         //String info[] = intro();
         //Object data[] = budget();
 
