a18e42e65a512a9c9bd70e473649b1af9bd56b27
YeshiNamkhai <yeshi.namkhai@gmail.com>
2019-09-10 21:55:41 +0200

    student

---
+++ Student.java
@@ -0,0 +1,24 @@
+class Student {
+    int id;
+    String name;
+    String gender;
+
+    public boolean nameUpdate(String newName) {
+        name = newName;
+        return true;
+    }
+
+    public static void main(String args[]) {
+        Student x = new Student();
+
+        x.id = 1000;
+        x.name = "joan";
+        x.gender = "male";
+
+        x.nameUpdate("John");
+
+        System.out.println(x.id);
+        System.out.println(x.name);
+        System.out.println(x.gender);
+    }
+}
