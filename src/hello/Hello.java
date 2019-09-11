7d3421eca360e47837f0c930c394ca79c0849b68
YeshiNamkhai <yeshi.namkhai@gmail.com>
2019-09-11 16:00:47 +0200

    last

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -1,7 +1,10 @@
 package src.hello;
 
 public class Hello{
-    
+    /**
+     * Main class
+     * @param args command line arguments
+     */
     public static void main(String[] args){
         System.out.println("Hello world!");
     }
