2c93beb8aa13f86b9dd462e6fb6a8d580d7ff24a
YeshiNamkhai <yeshi.namkhai@gmail.com>
2019-09-11 15:45:27 +0200

    si vai

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -1,5 +1,7 @@
+package src.hello;
+
 public class Hello{
-    //test
+    
     public static void main(String[] args){
         System.out.println("Hello world!");
     }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,4 @@
-package com.main;
+package src.main;
 
 class Main{
     /**
