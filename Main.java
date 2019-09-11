52a956152b5bb35889d77e3407024183a2d2d0ea
YeshiNamkhai <yeshi.namkhai@gmail.com>
2019-09-11 15:29:31 +0200

    update all

---
+++ Main.java
@@ -0,0 +1,25 @@
+class Main{
+    /**
+     * This is main method
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("args-size="+args.length);
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("args["+i+"]="+args[i]);
+        }
+
+        
+    }
+
+    /**
+     * This is a method to make a sum
+     * @param a first number
+     * @param b second number
+     * @return
+     */
+    public static int sum(int a,int b) {
+        return a+b;
+    }
+}
