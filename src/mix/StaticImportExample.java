506b4f9866c4b4fe603c88561dd610ecfcd700e4
YeshiNamkhai <yeshi.namkhai@gmail.com>
2019-09-13 17:14:21 +0200

    ok

---
+++ src/mix/StaticImportExample.java
@@ -0,0 +1,14 @@
+package src.mix;
+
+import static java.lang.Math.*;
+
+public class StaticImportExample {
+
+    public static void main(String[] args) {
+        
+    int min = min(5, 7);
+    System.out.println("min="+min);
+    System.out.println(Math.PI);
+    }
+}
+
